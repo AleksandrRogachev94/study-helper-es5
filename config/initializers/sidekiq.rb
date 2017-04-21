de4e390346f6aaeb17bd9396b026469490b49e87
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-04-21 14:25:05 -0400

    fix sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -16,6 +16,7 @@ if Rails.env.production?
         db_config = Rails.application.config.database_configuration[Rails.env]
         db_config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
         db_config['pool'] = pool_size
+        ActiveRecord::Base.establish_connection(db_config)
         Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
       end
     end
