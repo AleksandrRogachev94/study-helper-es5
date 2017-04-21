710823bd666051f8d44ace3b50495f11f1fb3763
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-04-21 14:09:44 -0400

    fix problems with pools and fix seeds. Deploying to heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,26 +1,23 @@
 if Rails.env.production?
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDISTOGO_URL'] || ENV['REDIS_URL'], size: 3 }
+    config.redis = { url: ENV['REDISTOGO_URL'] || ENV['REDIS_URL'], size: 1 }
   end
 
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDISTOGO_URL'] || ENV['REDIS_URL'], size: (Sidekiq.options[:concurrency] + 2) }
+    pool_size = ENV['SIDEKIQ_DB_POOL'] || (Sidekiq.options[:concurrency] + 2)
+    config.redis = { url: ENV['REDISTOGO_URL'] || ENV['REDIS_URL'], size: pool_size }
 
     Rails.application.config.after_initialize do
       Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
       ActiveRecord::Base.connection_pool.disconnect!
 
       ActiveSupport.on_load(:active_record) do
-        config = Rails.application.config.database_configuration[Rails.env]
-        config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
-        # config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
-        config['pool'] = 10 # 20 - 5*2 - 2(?)
-        ActiveRecord::Base.establish_connection(config)
-
+        db_config = Rails.application.config.database_configuration[Rails.env]
+        db_config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
+        db_config['pool'] = pool_size
         Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
       end
     end
   end
-
 end
