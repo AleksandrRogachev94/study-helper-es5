e3805aeab69b44172fcdd33984d1b7288592830f
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-04-21 00:30:15 -0400

    calculated and fixed number of processes/threads/pools

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 if Rails.env.production?
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDISTOGO_URL'] || ENV['REDIS_URL'], size: 2 }
+    config.redis = { url: ENV['REDISTOGO_URL'] || ENV['REDIS_URL'], size: 3 }
   end
 
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDISTOGO_URL'] || ENV['REDIS_URL'], size: 20 }
+    config.redis = { url: ENV['REDISTOGO_URL'] || ENV['REDIS_URL'], size: (Sidekiq.options[:concurrency] + 2) }
 
     Rails.application.config.after_initialize do
       Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
@@ -15,7 +15,7 @@ if Rails.env.production?
         config = Rails.application.config.database_configuration[Rails.env]
         config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
         # config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
-        config['pool'] = 16
+        config['pool'] = 10 # 20 - 5*2 - 2(?)
         ActiveRecord::Base.establish_connection(config)
 
         Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
