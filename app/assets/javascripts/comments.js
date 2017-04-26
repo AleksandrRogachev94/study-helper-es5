5895b8b838be25f677c8589039886527dfc207b1
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-04-26 14:08:16 -0400

    fix issue with comments time

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -8,7 +8,8 @@ function Comment(attributes) {
   for(prop in attributes.comment) {
     this[prop] = attributes.comment[prop]
   }
-  // if(this.created_at) this.created_at = Comment.formatDate(this.created_at)
+
+  if(this.created_at) this.created_at = Comment.formatDate(this.created_at)
 }
 
 // Instance methods
@@ -260,11 +261,11 @@ Comment.SubmitFormAJAX = function($form, success, fail) {
 //   })
 // }
 
-// Comment.formatDate = function(date) {
-//   var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-//   var formatted = new Date(date)
-//   return month[formatted.getMonth()] + " " + formatted.getDate() + ", " + formatted.getFullYear()
-// }
+Comment.formatDate = function(date) {
+  var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  var formatted = new Date(date)
+  return month[formatted.getMonth()] + " " + formatted.getDate() + ", " + formatted.getFullYear()
+}
 
 //------------------------------------------------------------------------
 // Documents Ready
